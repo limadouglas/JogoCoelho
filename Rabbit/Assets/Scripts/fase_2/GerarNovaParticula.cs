0dcf218006c753e1988ba50503a47bb1e01dfdf5
Douglas Henrique <lima.dougglas@gmail.com>
2016-12-30 03:19:10 -0200

    implementação de gotas de agua e lava na fase 2 e 5.

---
+++ Rabbit/Assets/Scripts/fase_2/GerarNovaParticula.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GerarNovaParticula : MonoBehaviour {
+
+	public GameObject particula;
+
+	void Update () {
+		if (GameObject.Find ("Player").transform.position.y < -2.3) {
+			Instantiate (particula).transform.position = GameObject.Find("Player").transform.position;
+		}
+	}
+}
